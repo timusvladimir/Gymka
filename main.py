b22a1060eb83eb48bdc786d6bd55c2b70d7e1367
timusvladimir <timusvladimir@gmail.com>
2023-12-30 18:30:31 +0300

    вывод в таблицу данных о тренировке и времени

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import sqlite3
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 import re
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from gymka_ui import Ui_MainWindow
@@ -11,9 +13,10 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.spisok = []
-        self.con = sqlite3.connect("2023.12.28-10.05.38_manual.db")
+        self.con = sqlite3.connect("2023.12.30-15.38.28_manual.db")
         self.pushButton_1.clicked.connect(self.run_button1)
         self.pushButton_2.clicked.connect(self.run_button2)
+        self.pushButton_3.clicked.connect(self.run_button3)
 
     def run_button1(self):
         try:
@@ -60,10 +63,10 @@ class Window(QMainWindow, Ui_MainWindow):
             try:
                 int(self.search_1)
                 if self.search_1 == '':
-                    request = "SELECT _id,name,startDateTime FROM training".format(self.search_1)
+                    request = "SELECT _id,name,startDateTime,setsAmount FROM training".format(self.search_1)
             except:
                 if self.search_1 == '':
-                    request = "SELECT _id,name,startDateTime FROM training".format(self.search_1)
+                    request = "SELECT _id,name,startDateTime,setsAmount FROM training".format(self.search_1)
 
             cur = self.con.cursor()
             result = cur.execute(request).fetchall()
@@ -83,6 +86,31 @@ class Window(QMainWindow, Ui_MainWindow):
         except Exception:
             self.label_4.setText('Введите значения полей')
 
+    def run_button3(self):
+        try:
+            # Линейная зависимость
+            x = np.linspace(0, 10, 50)
+            y1 = x
+            # Квадратичная зависимость
+            y2 = [i ** 2 for i in x]
+
+            # Построение графиков
+            plt.figure(figsize=(9, 9))
+            plt.subplot(2, 1, 1)
+            plt.plot(x, y1)  # построение графика
+            plt.title('Зависимости: y1 = x, y2 = x^2')  # заголовок
+            plt.ylabel('y1', fontsize=14)  # ось ординат
+            plt.grid(True)  # включение отображение сетки
+            plt.subplot(2, 1, 2)
+            plt.plot(x, y2)  # построение графика
+            plt.xlabel('x', fontsize=14)  # ось абсцисс
+            plt.ylabel('y2', fontsize=14)  # ось ординат
+            plt.grid(True)  # включение отображение сетки
+
+            plt.show()
+
+        except Exception:
+            self.label_4.setText('Введите значения полей')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
