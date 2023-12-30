827f9b2e17961117fbade0e3d89d3f81aa1e7912
timusvladimir <timusvladimir@gmail.com>
2023-12-30 16:02:33 +0300

    перевод времени из миллисекунд начиная с 01-01-1970

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 con = sqlite3.connect('2023.12.18-08.39.28_manual.db')
 cur = con.cursor()
@@ -9,3 +10,7 @@ for elem in result:
     print(elem[0])
 
 con.close()
+
+time_in_millis = 1703939445511
+dt = datetime.datetime.fromtimestamp(time_in_millis / 1000.0, tz=datetime.timezone.utc)
+print(dt)
